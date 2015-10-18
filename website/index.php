abb60baf9f1bc415cc22cace3580d5c8840cf5a3
Rece <PhilipReasa@gmail.com>
2015-10-18 14:38:33 -0700

    website update

--- website/index.php
+++ website/index.php
@@ -12,6 +12,15 @@
 	<script type="text/javascript" src="bower_components/bootstrap/dist/js/bootstrap.min.js"></script>
 
 	<script type="text/javascript" src="js/main.js"></script>
+	<script>
+	  (function(i,s,o,g,r,a,m){i['GoogleAnalyticsObject']=r;i[r]=i[r]||function(){
+	  (i[r].q=i[r].q||[]).push(arguments)},i[r].l=1*new Date();a=s.createElement(o),
+	  m=s.getElementsByTagName(o)[0];a.async=1;a.src=g;m.parentNode.insertBefore(a,m)
+	  })(window,document,'script','//www.google-analytics.com/analytics.js','ga');
+
+	  ga('create', 'UA-43619400-1', 'auto');
+	  ga('send', 'pageview');
+	</script>
 </head>
 
 <body>
@@ -33,6 +42,12 @@
     		<h1 class="col-sm-12 pad-top-m text-center">
 				Making Var_Dumps Beautiful
 	    	</h1>
+    	</div>
+
+    	<div class="row">
+    		<div class="col-sm-8 col-sm-offset-2 black-text text-center">
+    			<p>Complex var_dumps are hard to read unless you do extra work to format them. This chrome extension does that work for you, and turns nasty var_dumps into something much more beautifull</p>
+    		</div>
     	</div>
     	
 		<div class="row">
