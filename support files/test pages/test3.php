f7379d1dfb4e622d70dfcf854e8490ff9ccface0
Rece <PhilipReasa@gmail.com>
2016-07-30 23:57:20 -0700

    beefed up tests even more

--- support files/test pages/test3.php
+++ support files/test pages/test3.php
@@ -1,8 +1,14 @@
 <?php
 $a = [
-    "key1" => "value1",
-    "key2" => "string with \"quote\" and new lines \n testing...",
+    "key 1" => "value1",
+    "key 2" => "string with \"quote\" and new lines \n testing...",
     "value3",
-    "key4" => "value4"
+    "key 4" => "value4",
+    "5 lets get tough" => "\"\n\n\"\"\"\nthere should be something here",
+    "6 empty string" => "",
+    "7 single new line" => "\n",
+    "8 single quote" => "\"",
+    "9 new line + single space" => "\n ",
+    "key 10" => "I hope all of this works"
 ];
 var_dump($a);
