d18dc997b66890a347c2ae20586261164870a384
Rece <PhilipReasa@gmail.com>
2016-11-06 08:50:28 -0800

    changed folder names

--- support files/test pages/test4.php
+++
@@ -1,6 +0,0 @@
-<?php
-$test4 = [
-    "value has html" => "<a href='#'>link</a>",
-    "<a href='#'>key</a>" => "key has html",
-];
-var_dump($test4);
---
+++ support_files/test pages/Test8.php
@@ -0,0 +1,9 @@
+<?php
+$test5 = [
+    'Ford' => 'Fiesta',
+    'Toyota' => 'Starlet',
+    'Fiat' => 'Uno'
+];?>
+<p>
+<?php var_dump($test5); ?>
+</p>
