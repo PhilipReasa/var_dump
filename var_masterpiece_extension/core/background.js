8dec2734698a963b489e503adf5008fa2dc7c8fa
Rece <PhilipReasa@gmail.com>
2017-07-03 11:14:20 -0700

    cleaned up background,js

--- var_masterpiece_extension/core/background.js
+++ var_masterpiece_extension/core/background.js
@@ -1,30 +1,30 @@
-/*
-* Aux Functions 
-*/
-function getFromLocalStorage(localStorageName) {
-	if((localStorage[localStorageName] === undefined)||(localStorage[localStorageName] === "default")) {
-		return null;
-	} else {
-		return localStorage[localStorageName];
+/**
+ * This file handles interacting with the local storage to fetch and return information such as
+ * color values, autorun configuration, and the cascade configuration
+ *
+ * Example use: chrome.extension.sendRequest({action: "getAllOptions"}
+ */
+chrome.extension.onRequest.addListener((request, sender, sendResponse) => {
+	function getFromLocalStorage(localStorageName) {
+		if((localStorage[localStorageName] === undefined) || (localStorage[localStorageName] === "default")) {
+			return null;
+		} else {
+			return localStorage[localStorageName];
+		}
 	}
-}
 
-function getColors() {
-	var colors = {};
-	colors["int"] 	= getFromLocalStorage("intColor");
-	colors["float"] = getFromLocalStorage("floatColor");
-	colors["string"]= getFromLocalStorage("stringColor");
-	colors["null"] 	= getFromLocalStorage("nullColor");
-	colors["array"] = getFromLocalStorage("arrayColor");
-	colors["object"]= getFromLocalStorage("objectColor");
-	colors["bool"] 	= getFromLocalStorage("boolColor");
-	return colors;
-}
+	function getColors() {
+		var colors = {};
+		colors["int"] 	= getFromLocalStorage("intColor");
+		colors["float"] = getFromLocalStorage("floatColor");
+		colors["string"]= getFromLocalStorage("stringColor");
+		colors["null"] 	= getFromLocalStorage("nullColor");
+		colors["array"] = getFromLocalStorage("arrayColor");
+		colors["object"]= getFromLocalStorage("objectColor");
+		colors["bool"] 	= getFromLocalStorage("boolColor");
+		return colors;
+	}
 
-/*
-* The main code
-*/
-chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 	if (request.action === "getAllOptions") {
 	  colors = getColors();
 
@@ -32,6 +32,10 @@ chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
 		localStorage.autorun = "true"; //local storage only accepts strings
 	  }
 
-	  sendResponse({colors: colors, autorun:localStorage.autorun, cascade:localStorage.cascade});
+	  sendResponse({
+		  colors: colors,
+		  autorun:localStorage.autorun,
+		  cascade:localStorage.cascade
+	  });
     }
 });
